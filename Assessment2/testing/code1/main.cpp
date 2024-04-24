378e44f3ba0a6886ad74b15b13529e76867f50c6
fl-sll <edwardalvin@gmail.com>
2024-04-24 21:38:23 +0700

    added testing in main

--- Assessment2/testing/code1/main.cpp
+++ Assessment2/testing/code1/main.cpp
@@ -7,14 +7,15 @@
 #include "./../header1/coffeemaker.h"
 #include "./../header1/host.h"
 
+
 int main() {
-    // Create objects
+    //// Create objects
     Host host;
     Manager manager;
     FoodMaker foodMaker;
     CoffeeMaker coffeeMaker;
 
-    // Set relationships between objects
+    //// Set relationships between objects
     host.setManager(&manager);
     manager.setHost(&host);
     manager.setFoodMaker(&foodMaker);
@@ -22,13 +23,30 @@ int main() {
     foodMaker.setManager(&manager);
     coffeeMaker.setManager(&manager);
 
-    // Patron enters the cafe
+    //// Patron enters the cafe
     Patron patron("Alice");
 
-    // Patron gives order to the host
+    //// Patron gives order to the host
     Order order;
     order.addItem(new Food("Candy Bar"));
+    order.addItem(new Food("Chocolate Mousse"));
+    order.addItem(new BlackCoffee());
+    order.addItem(new WhiteCoffee());
+
+    //// host take order from patron and forwards to manager
     host.takeOrder(&patron, &order);
+
+    Patron patron1("John");
+
+    //// Patron gives order to the host
+    Order order1;
+    order1.addItem(new Food("Candy Bar"));
+    order1.addItem(new Food("Chocolate Mousse"));
+    order1.addItem(new BlackCoffee());
+    order1.addItem(new WhiteCoffee());
+
+    //// host take order from patron and forwards to manager
+    host.takeOrder(&patron1, &order1);
 
     return 0;
 }
--- Assessment2/testing/code1/order.cpp
+++ Assessment2/testing/code1/order.cpp
@@ -2,7 +2,7 @@
 
 void Order::addItem(MenuItem* item) {
     items.push_back(item);
-};
+}
 
 const std::vector<MenuItem*>& Order::getItems() const {
     return items;
