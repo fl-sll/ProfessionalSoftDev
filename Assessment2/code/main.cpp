26bb1b4c187ca8593f80e4b3b464459cfcfc1382
Alexander Tjiandra <91533574+JugBones@users.noreply.github.com>
2024-04-25 13:59:14 +0700

    Update main.cpp - try connect sandwich to interface

--- Assessment2/code/main.cpp
+++ Assessment2/code/main.cpp
@@ -6,6 +6,7 @@
 #include "./../header/food.h"
 #include "./../header/coffeemaker.h"
 #include "./../header/host.h"
+// #include "./../header/sandwich.h" 
 
 
 int main() {
@@ -23,7 +24,7 @@ int main() {
     foodMaker.setManager(&manager);
     coffeeMaker.setManager(&manager);
 
-    //// Patron enters the cafe
+    //// Patron 1 enters the cafe
     Patron patron("Alice");
 
     //// Patron gives order to the host
@@ -36,7 +37,7 @@ int main() {
     //// host take order from patron and forwards to manager
     host.takeOrder(&patron, &order);
 
-    Patron patron1("John");
+    Patron patron1("John"); // Patron 2 enters cafe
 
     //// Patron gives order to the host
     Order order1;
@@ -46,7 +47,18 @@ int main() {
     order1.addItem(new WhiteCoffee());
 
     //// host take order from patron and forwards to manager
-    host.takeOrder(&patron1, &order1);
+    // host.takeOrder(&patron2, &order2);
+
+    // Patron patron2("Josh pecinta onigiri");
+
+    //// Patron gives order to the host
+    // Order order2;
+    // order2.addItem(new Food("Candy Bar"));
+    // order2.addItem(new BlackCoffee());
+    // order2.addItem(new SubSandwich()); // Add a SubSandwich to the order
+
+    //// Host takes order from patron and forwards to manager
+    // host.takeOrder(&patron2, &order2);
 
     return 0;
 }
